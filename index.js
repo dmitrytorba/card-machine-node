021f4e75d36f900cf39ae1feceb28aacb724d25b
Dmitry Torba <dmitrytorba@gmail.com>
2021-01-24 10:05:33 -0800

    start

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { postgraphile } = require("postgraphile");
+
+// const pass = process.env.HABIT_PSQL_PASS;
+// if (!pass) {
+//   console.log("Please set HABIT_PSQL_PASS");
+//   return;
+// }
+
+const app = express();
+
+app.use(
+  postgraphile(
+    process.env.DATABASE_URL || `postgres:///habits`,
+    "public",
+    {
+      watchPg: true,
+      graphiql: true,
+      enhanceGraphiql: true,
+    }
+  )
+);
+
+app.listen(process.env.PORT || 3001);
